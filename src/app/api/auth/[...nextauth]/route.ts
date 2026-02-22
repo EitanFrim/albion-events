b90be3fc761525410080cc26ac0970591ee5193f
Eitan Frim <eatanfrimerman@gmail.com>
2026-02-22 18:37:30 +0200

    fix: force-dynamic on nextauth route to fix build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
-import NextAuth from 'next-auth'
-import { authOptions } from '@/lib/auth'
+export const dynamic = 'force-dynamic';
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+import NextAuth from 'next-auth';
+import { authOptions } from '@/lib/auth'; // adjust this import to match your setup
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
